7ee185ca9c3f41cb68d2961b8fa08ebfa6335cde
Nicole Dow <nikki.dow71@gmail.com>
2019-08-01 17:12:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,7 @@ class Scraper
   #instantiates Course objects and gives them a title, schedule, description attribute scraped from the page
   def make_courses
     doc = get_courses
-    doc.each do |ele|
+    doc.each do |ele, class|
       binding.pry
     end
   end
