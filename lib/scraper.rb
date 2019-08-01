097da05df919450988eec992e28f1da121dd38dd
Nicole Dow <nikki.dow71@gmail.com>
2019-08-01 16:57:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,28 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
 require_relative './course.rb'
 
 class Scraper
+  
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+  
+  #uses CSS selector to grab HTML elements containing a course
+  def get_courses
+    get_page.css("h2")
+  end
+  
+  #instantiates Course objects and gives them a title, schedule, description attribute scraped from the page
+  def make_courses
+    doc = get_courses
+    doc.each do |ele|
+      binding.pry
+    end
+  end
+  
   
     def print_courses
     self.make_courses
